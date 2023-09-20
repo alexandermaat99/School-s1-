90791b6089c4ef732822f0a092a1e483ec2e56a9
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-09-20 00:03:53 -0600

    7.9Practice

--- is403/Week 2/6.7Practice/67.js
+++ is403/Week 2/6.7Practice/67.js
@@ -1,4 +1,4 @@
-alert("Test")
+
 
 // Homework Average = 10%
 // Midterm 1 Score = 20%
@@ -8,16 +8,59 @@ alert("Test")
 // Project 2 Score = 7.5%
 // Project 3 Score = 12.5%
 
-sName = prompt("What is your name?");
+// A is 94 and above
+// A- is 90 to 93
+// B+ is 87 to 89
+// B is 83 to 86
+// B- is 80 to 82
+// C+ is 77 to 79
+// C is 73 to 76
+// C- is 70 to 72
+// D+ is 67 to 69
+// D is 63 to 66
+// D- is 60 to 62
+// Anything else is an E
 
-fHomeworkAvg = parseFloat(prompt("What is your homework average?"));
-iMidterm1 = parseInt(prompt("What is your midterm 1 score?"));
-imidterm2 = parseInt(prompt("What is your midterm 2 score?"));
-iFinalExam = parseInt(prompt("What is your final exam score?"));
-fProject1 = parseFloat(prompt("What is your project 1 score?"));
-fProject2 = parseFloat(prompt("What is your project 2 score?"));
-fProject3 = parseFloat(prompt("What is your project 3 score?"));
 
-fFinalGrade = (fHomeworkAvg * .1) + (iMidterm1 * .2) + (imidterm2 * .2) + (iFinalExam * .25) + (fProject1 * .05) + (fProject2 * .075) + (fProject3 * .125);
 
-alert(sName.toUpperCase() + " recieved a " + fFinalGrade.toFixed() + "% for the final grade.");
+var sName = prompt("What is your name?");
+
+var fHomeworkAvg = parseFloat(prompt("What is your homework average?"));
+var iMidterm1 = parseInt(prompt("What is your midterm 1 score?"));
+var iMidterm2 = parseInt(prompt("What is your midterm 2 score?"));
+var iFinalExam = parseInt(prompt("What is your final exam score?"));
+var fProject1 = parseFloat(prompt("What is your project 1 score?"));
+var fProject2 = parseFloat(prompt("What is your project 2 score?"));
+var fProject3 = parseFloat(prompt("What is your project 3 score?"));
+
+var fFinalGrade = (fHomeworkAvg * 0.1) + (iMidterm1 * 0.2) + (iMidterm2 * 0.2) + (iFinalExam * 0.25) + (fProject1 * 0.05) + (fProject2 * 0.075) + (fProject3 * 0.125);
+
+var sLetterGrade = "";
+
+if (fFinalGrade >= 94) {
+    sLetterGrade = "A";
+} else if (fFinalGrade >= 90) {
+    sLetterGrade = "A-";
+} else if (fFinalGrade >= 87) {
+    sLetterGrade = "B+";
+} else if (fFinalGrade >= 83) {
+    sLetterGrade = "B";
+} else if (fFinalGrade >= 80) {
+    sLetterGrade = "B-";
+} else if (fFinalGrade >= 77) {
+    sLetterGrade = "C+";
+} else if (fFinalGrade >= 73) {
+    sLetterGrade = "C";
+} else if (fFinalGrade >= 70) {
+    sLetterGrade = "C-";
+} else if (fFinalGrade >= 67) {
+    sLetterGrade = "D+";
+} else if (fFinalGrade >= 63) {
+    sLetterGrade = "D";
+} else if (fFinalGrade >= 60) {
+    sLetterGrade = "D-";
+} else {
+    sLetterGrade = "E";
+}
+
+alert(sName.toUpperCase() + " recieved a " + fFinalGrade.toFixed() + "% for the final grade which is a(n) " + sLetterGrade + " in the course.");
