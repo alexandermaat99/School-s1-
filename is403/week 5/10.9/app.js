c012bd000b22934479dbd70c529c681009a7cc69
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-10-09 09:02:20 -0600

    idk

--- is403/week 5/10.9/app.js
+++ is403/week 5/10.9/app.js
@@ -1,2 +1,8 @@
 //alexander maat
 //section 001
+
+let aNames = [];
+let sMenu =
+  "1 - Add to end\n2 - Remove from end \n3 - Add to beginning\n4 - Remove from beginning\n5 - Exit";
+
+iInput = parseInt(prompt(sMenu));
