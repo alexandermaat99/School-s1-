7c40cdd70846eac8efe70638300079f4eef960e6
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-10-25 21:12:11 -0600

    add/ Yahtzee Dialed1

--- is403/week 8/Homework7/app.js
+++ is403/week 8/Homework7/app.js
@@ -15,6 +15,7 @@ function rollDie(dieNumber, targetNumber) {
 function rollTheDice() {
   let numberInput = document.getElementById("numberInput");
   let targetNumber = parseInt(numberInput.value);
+  let numberOfDie = parseInt(newDieNumber.value);
 
   if (targetNumber < 1 || targetNumber > 6) {
     alert("Please enter a number between 1 and 6.");
@@ -22,16 +23,26 @@ function rollTheDice() {
     return;
   }
 
-  let totalRolls = 0;
-  let diceResults = [];
+  // Clear the previous results
+  let diceResultsList = document.getElementById("diceResults");
+  diceResultsList.innerHTML = "";
 
+  // Clear the previous dice images
   for (let dieNumber = 1; dieNumber <= 5; dieNumber++) {
-    let rolls = rollDie(dieNumber, targetNumber);
-    totalRolls += rolls;
-    diceResults.push(`Dice ${dieNumber} Rolls = ${rolls}`);
+    document.getElementById(`dice${dieNumber}`).src = "";
   }
 
-  document.getElementById("diceResults").textContent = diceResults.join(", ");
+  let totalRolls = 0;
+
+  for (let dieNumber = 1; dieNumber <= numberOfDie; dieNumber++) {
+    let rolls = rollDie(dieNumber, targetNumber);
+    totalRolls += rolls;
+
+    let listItem = document.createElement("p");
+    listItem.textContent = `Dice ${dieNumber} Rolls = ${rolls}`;
+    diceResultsList.appendChild(listItem);
+  }
+
   document.getElementById(
     "totalRolls"
   ).textContent = `Total Rolls = ${totalRolls}`;
