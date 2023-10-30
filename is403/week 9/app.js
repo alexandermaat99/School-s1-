447847ae4ab2ad31460d77791dddbebec1de7e45
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-10-30 08:42:04 -0600

    add/ classworkOct30

---
+++ is403/week 9/app.js
@@ -0,0 +1,11 @@
+//three attrubtres connected to Student class name
+//use "new" then specifiy class name "Student"
+//finds constructor method
+
+class Student {
+  constructor(id, fName, lName) {
+    this.netId = id;
+    this.firstName = fName;
+    this.lastName = lName;
+  }
+}
