8edaa5ea46d6b93d70a6dc0d4b36d9fe9e4f8944
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-11-10 11:33:24 -0700

    add/ finsihedHW9

--- is403/week10/index.js
+++ is403/week10/index.js
@@ -1,7 +1,12 @@
+//alexander maat
+//section 001 group 14
+//this created 2 routs in express
+
 let express = require("express");
 
 let app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("This is my first node application"));
+app.get("/help", (req, res) => res.send("Please contact tech support"));
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log("Server is runnning"));
