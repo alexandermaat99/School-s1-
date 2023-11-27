960c2b138dcd65de7811919d8b67c6841a1f2abd
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-11-27 09:18:01 -0700

    add/ nodeAppCreated

--- is403/mytravels/index.js
+++ is403/mytravels/index.js
@@ -10,4 +10,15 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
-const knex = require("knex")({});
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    host: localhost,
+    user: "postgres",
+    password: "admin",
+    database: "bucket_list",
+    port: 5432,
+  },
+});
+
+app.listen(port, () => console.log("Listening"));
--- is403/week 3/buildInFunctionsPractice/bif.js
+++
@@ -1,7 +0,0 @@
-let input = prompt("Enter a phrase: ");
-let search = prompt("Which part would you like to replace?: ");
-let replace = prompt("What would you like to replace it with?: ");
-
-newPhrase = input.replace(search, replace).trim();
-
-alert(newPhrase);
