be890d51a492c64e68f7d806e348a86c78dadc5e
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-12-02 12:38:56 -0700

    RDS Settings

--- is403/mytravels/index.js
+++ is403/mytravels/index.js
@@ -13,11 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 const knex = require("knex")({
   client: "pg",
   connection: {
-    host: "localhost",
-    user: "postgres",
-    password: "admin",
-    database: "bucket_list",
-    port: 5432,
+    host: process.env.RDS_HOSTNAME || "localhost",
+    user: process.env.RDS_USERNAME || "postgres",
+    password: process.env.RDS_PASSWORD || "admin",
+    database: process.env.RDS_DB_NAME || "bucket_list",
+    port: process.env.RDS_PORT || 5432,
   },
 });
 
