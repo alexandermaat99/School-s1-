f3d464e91369c69150427ab0a9141bfb1cea378d
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-12-02 18:23:18 -0700

    newDomains

--- is403/mytravels/.platform/hooks/postdeploy/00_get_certificate.sh
+++ is403/mytravels/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 sudo certbot -n -d amaatmytravels.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email aamaat99@gmail.com
+sudo certbot -n -d skillcheck4.amaat.theprintedpot.com --nginx --agree-tos --email aamaat99@gmail.com
+sudo certbot -n -d skillcheck4-2.amaat.theprintedpot.com --nginx --agree-tos --email aamaat99@gmail.com
