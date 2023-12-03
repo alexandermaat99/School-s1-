470367367ea6cb5b7dc5768ac8255d42b38d6f88
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-12-02 17:54:54 -0700

    newFix

--- is403/mytravels/.platform/hooks/postdeploy/00_get_certificate.sh
+++ is403/mytravels/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo certbot -n -d amaatmytravels.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email aamaat99@gmail.com
+sudo certbot -n -d amaatmytravels.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email aamaat99@gmail.com
