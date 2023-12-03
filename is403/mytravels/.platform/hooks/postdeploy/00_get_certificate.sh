3fd9cf64c0593d7fa6758b3e6f1d5138c65acfbd
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2023-12-02 17:40:33 -0700

    email

--- is403/mytravels/.platform/hooks/postdeploy/00_get_certificate.sh
+++ is403/mytravels/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d YOURDOMAINHERE --nginx --agree-tos --email YOUREMAILHERE
+sudo certbot -n -d http://amaatmytravels.us-east-1.elasticbeanstalk.com/ --nginx --agree-tos --email aamaat99@gmail.com
